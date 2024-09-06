ec99d2ab17b782526ed39ac6c93230a9cbcf2af2
Aaron Raymund Paguirigan <paguiriganaaronraymund@gmail.com>
2024-09-06 20:33:20 +0800

    initial commit with app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+let myname = "Aaron Raymund R Paguirigan"
+console.log("my name is", myname);
+
+console.log("Using Variables");
+let name = "Aaron";
+let age = "19";
+console.log("Name:", name);
+console.log("Age:", age);
+
+console.log("Using Numbers");
+const number1 = 15;
+const number2 = 15;
+const sum number1 + number2;
+
+console.log(sum);
+
+console.log("Using Boleans");
+let progamming = true;
+console.log("I love Programming?", progamming);
+
+console.log("Using Comparison Operator");
+let z =13;
+let q =15;
+
+if (z == q){
+    console.log(a, 'and', b , 'is equal');
+} else { 
+    console.log(a, 'and', b , "is not equal");
+}    
